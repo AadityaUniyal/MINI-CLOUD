28687103e9ebc34a1040346fdcd914bf3e95b152
Aaditya Uniyal <aaditya.uniyal22@gmail.com>
2026-03-26 10:45:26 +0530

    Update Bucket.java

--- src/main/java/com/minicloud/model/Bucket.java
+++ src/main/java/com/minicloud/model/Bucket.java
@@ -22,5 +22,10 @@ public class Bucket {
     @Column(nullable = false)
     private String owner; // username
 
+    // S3-like Metadata
+    private String region; // us-east-1
+    private String accessControl; // Private
+    private String arn; // arn:aws:s3:::name
+
     private LocalDateTime createdAt;
 }
