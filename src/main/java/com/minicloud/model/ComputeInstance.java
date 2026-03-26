919b1921db551cfe5d0cc9aa1a244dec5266a093
Aaditya Uniyal <aaditya.uniyal22@gmail.com>
2026-03-26 10:45:54 +0530

    Update ComputeInstance.java

--- src/main/java/com/minicloud/model/ComputeInstance.java
+++ src/main/java/com/minicloud/model/ComputeInstance.java
@@ -3,6 +3,7 @@ package com.minicloud.model;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "compute_instances")
@@ -23,7 +24,20 @@ public class ComputeInstance {
     private String image; // e.g., tomcat:latest
     private String status; // RUNNING, STOPPED, TERMINATED
     private String owner; // Username of the owner
-    private String publicIp; // Simulated IP (e.g., localhost:8080)
+    
+    // AWS-like Metadata
+    private String instanceType; // e.g., t2.micro, t2.small
+    private String region; // e.g., us-east-1, ap-south-1
+    private String availabilityZone; // e.g., us-east-1a
+    private String publicIp; // Simulated public IP
+    private String privateIp; // Simulated private IP (172.31.x.x)
+    private String vpcId; // simulated vpc-xxxxx
+    
+    @ElementCollection
+    private List<String> securityGroups;
+    private String keyPairName;
+    
+    private Integer hostPort;
     
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
